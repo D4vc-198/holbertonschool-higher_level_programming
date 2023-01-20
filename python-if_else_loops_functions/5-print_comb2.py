62faab827414db3b4799ea80ac8569bf2b4ab762
d4vc-198 <contactodiegoavc@gmail.com>
2023-01-20 13:15:04 -0600

    5.00...99

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,10 +1,3 @@
 #!/usr/bin/python3
-for n in range(0, 100):
-    if n <= 9:
-        print("0{}".format(n), end=', ')
-    else:
-        if n <= 98:
-            print("{}".format(n), end = ', ')
-        else:
-            print("{}".format(n), end='')
+print(", ".join("{:02d}".format(n) for n in range(0, 100)))
 
