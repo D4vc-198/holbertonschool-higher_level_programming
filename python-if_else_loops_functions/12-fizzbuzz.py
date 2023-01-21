a005a5224bd5bd12b1132559eb53e220afc4f95b
d4vc-198 <contactodiegoavc@gmail.com>
2023-01-20 21:56:45 -0600

    12.Fizz Buzz

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 def fizzbuzz():
     for n in range(1, 100):
-        if n % 3 == 0:
+        if (n % 3) == 0:
             if n % 15 == 0:
                 print(" FizzBuzz")
             else:
                 print(" Fizz")
-        elif n % 5 == 0:
-            if n % 15 == 0:
+        elif (n % 5) == 0:
+            if (n % 15) == 0:
                 print(" FizzBuzz")
             else:
                 print(" Buzz")
