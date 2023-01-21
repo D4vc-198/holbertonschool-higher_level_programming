3972c3b01e1b7469d877fa222f055e54f46bbb06
d4vc-198 <contactodiegoavc@gmail.com>
2023-01-20 22:25:42 -0600

    3. Infinite addition

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    result = 0
-    if (len(sys.argv) > 1):
-        for i in range(1, len(sys.argv)):
-            result += (int(sys.argv[i]))
-    print("{:d}".format(result))
+    num = len(sys.argv)
+    sum = 0
+    if num > 1:
+        for i in range(1, num):
+            sum += int(sys.argv[i])
+        print("{}".format(sum))
+    else:
+        print("{}".format(sum))
