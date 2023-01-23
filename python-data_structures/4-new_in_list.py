b738268de2c70716544e10bd28aeca0cc2207063
d4vc-198 <contactodiegoavc@gmail.com>
2023-01-23 13:20:08 -0600

    4. Replace in a copy

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -2,7 +2,8 @@
 def new_in_list(my_list, idx, element):
     copyOfList = my_list[:]
 
-    if idx > 0 or idx < len(my_list) - 1:
+    if idx < 0 or idx > len(my_list) - 1:
         return copyOfList
     else:
         copyOfList[idx] = element
+        return copyOfList
