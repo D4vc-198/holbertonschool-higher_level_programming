2df837b3c47db9e05d57381e5b83cbed8b78c558
d4vc-198 <contactodiegoavc@gmail.com>
2023-01-25 21:04:47 -0600

    3. Present in both

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    common = []
-    for i, n in zip(set_1, set_2):
-        if n == i:
-            common.append(n)
-    return common
+    return set_1 & set_2
