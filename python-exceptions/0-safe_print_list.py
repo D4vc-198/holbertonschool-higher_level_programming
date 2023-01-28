ab2c6f6d7bd3d0d9e39b05a35d78edd150852c2b
d4vc-198 <contactodiegoavc@gmail.com>
2023-01-27 23:21:57 -0600

    0.Safe list printing

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -3,7 +3,7 @@ def safe_print_list(my_list=[], x=0):
     counter = 0
     for n in range(x):
         try:
-            print("{}".format(my_list[n]), end="")
+            print(my_list[n], end="")
             counter += 1
         except:
             break
