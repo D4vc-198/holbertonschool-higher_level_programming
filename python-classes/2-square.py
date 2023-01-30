77dd6afdf2ffce7412e83fabcd2c6028e57cfc4d
d4vc-198 <contactodiegoavc@gmail.com>
2023-01-30 15:47:12 -0600

    2.Size validation

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,6 +1,19 @@
 #!/usr/bin/python3
+""" Module Square """
+
+
 class Square:
+    """ Square Class
+    Attributes:
+        size (int): size of square
+    """
     def __init__(self, size=0):
+        """
+        Initialize Methode
+
+        Args:
+            size (int): Size of square
+        """
         if type(size) != int:
             raise TypeError("size must be an integer")
         elif size < 0:
