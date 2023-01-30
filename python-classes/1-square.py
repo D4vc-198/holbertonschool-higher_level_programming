9fd73ce2a4c5fc9eff56d77e7c732fc44d94630f
d4vc-198 <contactodiegoavc@gmail.com>
2023-01-30 15:18:11 -0600

    1.Square with size

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
 class Square:
+    """ Square Class """
+
     def __init__(self, size):
+        """
+        Initialize methode
+
+        Args:
+            size (int): size of square
+        """
         self.__size = size
