9863b5ec95f1e21ac7728ae64504aec61c3366a1
d4vc-198 <contactodiegoavc@gmail.com>
2023-01-31 22:36:54 -0600

    6.Coordinates of a square

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -60,10 +60,11 @@ class Square:
         Args:
             position: postion of square in 2D space
         """
-        if value[0] and value[1] < 0:
+        if value[0] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
-        else:
-            self.__position = value
+        if value[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
 
     def area(self):
         """
