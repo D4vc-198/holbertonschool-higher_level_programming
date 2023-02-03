8b0873c6378036a07a8714779d825d807a617d55
d4vc-198 <contactodiegoavc@gmail.com>
2023-02-02 22:14:48 -0600

    0.Integers addition

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Add integer or float
+"""
+
+
+def  add_integer(a, b=98):
+    """
+    Add two integer "a" and "b"
+    Args:
+        a (int or float): first value
+        b (int or float): second value
+    Return:
+        Sum "a" and "b"
+    """
+    if type(a) not in[int, float]:
+        raise TypeError("a must be an integer")
+    if type(b) not in [int, float]:
+        raise TypeError("b must be an integer")
+
+    return int(a) + int(b)
