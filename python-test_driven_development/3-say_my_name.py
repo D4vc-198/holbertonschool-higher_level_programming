308acbedf8fc98b9bff3cf505cd31c84b880b2d0
d4vc-198 <contactodiegoavc@gmail.com>
2023-02-03 16:09:48 -0600

    2.Say my name

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,7 +1,20 @@
 #!/usr/bin/python3
+"""
+Module print name
+"""
+
+
 def say_my_name(first_name, last_name=""):
+    """
+    print name and last name
+    Args:
+        first_name (str): first name
+        last_name (str): last name
+    Raise:
+        TypeError: if first or last name is not string
+    """
     if type(first_name) is not str:
         raise TypeError("first_name must be a string")
     if type(last_name) is not str:
         raise TypeError("last_name must be a string")
-    return (first_name + " " + last_name)
+    print("My name is {} {}".format(first_name, last_name))
