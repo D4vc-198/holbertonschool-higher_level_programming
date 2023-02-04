e5f5c4897768996eb2b032e45e17ec916442cf5e
d4vc-198 <contactodiegoavc@gmail.com>
2023-02-03 22:32:17 -0600

    5.Max integer - Unittest

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -10,8 +10,24 @@ class TestMaxInteger(unittest.TestCase):
         self.assertEqual(max_integer(list_ord), 5)
 
     def test_not_order(self):
-        list_not_ord = [2,5,3,4]
+        list_not_ord = [5,1,3,4,2]
         self.assertEqual(max_integer(list_not_ord), 5)
+
+    def test_negative_num(self):
+        list_n = [1, -2, 3, -4,]
+        self.assertEqual(max_integer(list_n), 3)
+
+    def test_only_negative_num(self):
+        list_n = [-1, -2, -3, -4, -5]
+        self.assertEqual(max_integer(list_n), -1)
+
+    def test_one_element(self):
+        list_n = [1]
+        self.assertEqual(max_integer(list_n), 1)
+
+    def test_empty(self):
+        list_n = []
+        self.assertEqual(max_integer(list_n), None)
 
 if __name__ == '__main__':
     unittest.main()
