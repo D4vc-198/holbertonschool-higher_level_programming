c801fc70fc5ac3a01023b93871b3616db5ad065c
d4vc-198 <contactodiegoavc@gmail.com>
2023-02-09 15:10:23 -0600

    1.My list

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -5,6 +5,5 @@
 class MyList(list):
     """ MyList class """
     def print_sorted(self):
-        sorted_list = self[:]
         """ print sorted list"""
-        print(sorted_list.sort())
+        print(sorted(self))
