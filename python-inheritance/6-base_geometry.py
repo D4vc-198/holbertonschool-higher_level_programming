3c198a30a3e08cb208b3f64f8510ba90fc359d73
d4vc-198 <contactodiegoavc@gmail.com>
2023-02-10 10:47:28 -0600

    6.Improve Geometry

--- python-inheritance/6-base_geometry.py
+++ python-inheritance/6-base_geometry.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
-""" Geometry represent """
+""" Geometry Module """
 
 
 class BaseGeometry:
+    """ represent geometry class"""
+
     def area(self):
+        """ "geometry area """
         raise Exception("area() is not implemented")
