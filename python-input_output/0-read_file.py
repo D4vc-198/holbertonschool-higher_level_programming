79ee4ea1d7751a3ba098d494314705ef99674296
d4vc-198 <contactodiegoavc@gmail.com>
2023-02-10 14:37:20 -0600

    0.Read file

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Read file module """
+
+def read_file(filename=""):
+    """
+    Read and print content file
+    """
+    with open(filename, encoding="utf-8") as f:
+        print(f.read())
