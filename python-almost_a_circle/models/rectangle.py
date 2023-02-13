9047689f2b297023b6c6e282d4970270b1ac4522
d4vc-198 <contactodiegoavc@gmail.com>
2023-02-13 17:43:26 -0600

    2.First Rectangle

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -6,6 +6,10 @@ from models.base import Base
 class Rectangle(Base):
     """ represent a Rectangle """
     def __init__(self, width, height, x=0, y=0, id=None):
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
         super().__init__(id)
 
     """ Width """
