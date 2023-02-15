1157639aae149e969f67b8927cd8b89c8828b192
d4vc-198 <contactodiegoavc@gmail.com>
2023-02-15 14:18:10 -0600

    test base modified

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -1,155 +1,139 @@
 #!/usr/bin/python3
-"""Unittest for base({..]) after task 1"""
+"""
+Unittest module for Base class.
+"""
 import unittest
-from models.base import Base
+from models.base import Base as Base
+from models.rectangle import Rectangle as Rectangle
+from models.square import Square as Square
 
 
-class TestBase(unittest.TestCase):
-    """Tests `Base` class."""
-
+class BaseTests(unittest.TestCase):
+    """
+    Class BaseTests that provides unit testing for the `Base` class.
+    """
     @classmethod
     def setUpClass(cls):
-        """Alerts user to any instances of tested classes or subclasses not
-        already cleaned up.
         """
-        b_obj = Base._Base__nb_objects
-        b_tobj = Base._Base__true_nb_objects
-        b_ids = Base._Base__assigned_ids
-        if b_obj > 1:
-            print('testBase: previous Base counter not reset,' +
-                  'now at: {}'.format(b_obj))
-        if b_tobj > 1:
-            print('testBase: previous total Base counter not reset,' +
-                  'now at: {}'.format(b_tobj))
-        if len(b_ids) > 0:
-            print('testBase: previous Base ids still potentially in' +
-                  'use: {}'.format(b_ids))
+        setUp method for testing the Base class.
+        """
+        cls.base1 = Base()
+        cls.rect1 = Rectangle(1, 1, 0, 0, 200)
+        cls.square1 = Square(1, 0, 0, 300)
 
-    def tearDown(self):
-        """Reinitializes obejct iterator and set of assigned ids.
+    @classmethod
+    def tearDownClass(cls):
         """
-        b_ids = Base._Base__assigned_ids
-        Base._Base__nb_objects = 0
-        b_obj = Base._Base__nb_objects
-        Base._Base__true_nb_objects = 0
-        b_tobj = Base._Base__true_nb_objects
-        b_ids.clear()
-        if b_obj > 1:
-            print('testBase: Base counter not reset, now at: {}'.format(b_obj))
-        if b_tobj > 1:
-            print('testBase: total Base counter not reset,' +
-                  ' now at: {}'.format(b_tobj))
-        if len(b_ids) > 0:
-            print('testBase: Base ids still potentially in' +
-                  'use: {}'.format(b_ids))
+        tearDown method for testing the Base class.
+        """
+        del cls.base1
+        del cls.rect1
+        del cls.square1
+        return super().tearDownClass()
 
-    def test_id(self):
-        """Task 1. Base class
-        According to project specs, `id` can be assumed to be an integer,
-        so TypeError testing omitted.
+    def test_init(self):
         """
-        # ValueError: assigning id of 0
-        self.assertRaises(ValueError, Base, 0)
-        # ValueError: assigning negative ids
-        self.assertRaises(ValueError, Base, -5)
-        # Normal use: Assigning postive id
-        b1 = Base(2)
-        self.assertEqual(b1.id, 2)
-        self.assertEqual(b1.serial, 1)
-        # No arg defaults to id = count of instances w/o manually assigned id
-        b2 = Base()
-        self.assertEqual(b2.id, 1)
-        self.assertEqual(b2.serial, 2)
-        # Further instance w/o arg continues pattern, even if id already used
-        b3 = Base()
-        self.assertEqual(b3.id, 2)
-        self.assertEqual(b3.serial, 3)
-        # Manually assigning id that has already automatically been assigned
-        b4 = Base(2)
-        self.assertEqual(b1.id, 2)
-        self.assertEqual(b1.serial, 1)
-        self.assertEqual(b3.id, 2)
-        self.assertEqual(b3.serial, 3)
-        self.assertEqual(b4.id, 2)
-        self.assertEqual(b4.serial, 4)
+        Method for testing initialization of the Base class.
+        """
+        self.assertEqual(self.base1.id, 1)
+
+        self.base2 = Base(2)
+        self.assertEqual(self.base2.id, 2)
+
+        self.base3 = Base()
+        self.assertEqual(self.base3.id, 2)
+
+        self.assertEqual(self.base2.id, self.base3.id)
+
+        """
+        There is no sanitization for id: it can be any type,
+        though it is assumed to be an integer
+        <<Possibly Not Required>>
+        """
+        self.base4 = Base("stuff")
+        self.assertEqual(self.base4.id, 'stuff')
+
+        del self.base2
+        del self.base3
+        del self.base4
 
     def test_to_json_string(self):
-        """15. Dictionary to JSON string"""
-        # Normal use: 1 arg (list) of (dict)
-        d_list = [{'id': 10}, {'id': 15}]
-        self.assertEqual(Base.to_json_string(d_list),
-                         '[{"id": 10}, {"id": 15}]')
-        # Empty list
-        self.assertEqual(Base.to_json_string([]), '[]')
-        # None list
-        self.assertEqual(Base.to_json_string(None), '[]')
-        # deprecated use: list not of dicts
-        lst = [2, 3, 4]
-        self.assertEqual(Base.to_json_string(lst), '[2, 3, 4]')
-        # TypeError: no args
-        self.assertRaises(TypeError, Base.to_json_string)
-        d_list2 = [{'id': 20}]
-        # TypeError: 2+ args
-        self.assertRaises(TypeError, Base.to_json_string, d_list, d_list2)
+        """
+        Method for testing the to_json_string method in the Base
+        class.
+        """
+        dictionary_r = self.rect1.to_dictionary()
+        json_dictionary_r = self.rect1.to_json_string([dictionary_r])
+        dicStr = '[{"x": 0, "y": 0, "id": 200, "height": 1, "width": 1}]'
+        self.assertEqual(json_dictionary_r, dicStr)
+
+        dictionary_s = self.square1.to_dictionary()
+        json_dictionary_s = self.square1.to_json_string([dictionary_s])
+        dicStr = '[{"id": 300, "x": 0, "size": 1, "y": 0}]'
+        self.assertEqual(json_dictionary_s, dicStr)
+
+        self.assertRaises(TypeError, self.square1.to_json_string, dictionary_s)
+        self.assertRaises(TypeError, self.square1.to_json_string, [1])
+
+        dicList = [self.square1.to_dictionary(), self.rect1.to_dictionary()]
+        dicStr = '[{"id": 300, "x": 0, "size": 1, "y": 0}, ' \
+            + '{"x": 0, "y": 0, "id": 200, "height": 1, "width": 1}]'
+        self.assertEqual(self.square1.to_json_string(dicList), dicStr)
+
+        self.assertEqual(self.square1.to_json_string(None), "[]")
+        self.assertEqual(self.square1.to_json_string([]), "[]")
 
     def test_save_to_file(self):
-        """16. JSON string to file
-        AttributeError: .to_dictionary() not in `Base`, only Square/Rectangle.
-        Testing of `save_to_file` done in `TestRectangle` and `TestSquare`.
         """
-        # Empty list
-        # None list
-        # arg not an iterable type
-        # arg not a list, but tuple or set
-        # arg list contents not Squares or Rectangles (no to_dictionary())
-        # class of list objects does not match class that called save_to_file
-        # no args
-        # 2+ args
+        Method for testing the save_to_file method in the Base
+        class.
+        """
         pass
 
     def test_from_json_string(self):
-        """17. JSON string to dictionary"""
-        # Normal use: 1 arg (str)
-        a = Base.from_json_string('[{"id": 10}, {"id": 15}]')
-        self.assertEqual(a, [{'id': 10}, {'id': 15}])
-        # Empty str
-        b = Base.from_json_string('')
-        self.assertEqual(b, [])
-        # None str
-        c = Base.from_json_string(None)
-        self.assertEqual(c, [])
-        # TypeError: no args
-        self.assertRaises(TypeError, Base.from_json_string)
-        # TypeError: 2+ args
-        self.assertRaises(TypeError, Base.from_json_string, a, b)
+        """
+        Method for testing the from_json_string method in the Base
+        class.
+        """
+        list_input = [
+            {'id': 89, 'width': 10, 'height':4},
+            {'id': 7, 'size': 3}
+        ]
+        json_list_input = Rectangle.to_json_string(list_input)
+        list_output = Base.from_json_string(json_list_input)
+        self.assertEqual(list_output, list_input)
+
+        json_list_input = Rectangle.to_json_string([])
+        list_output = Base.from_json_string(json_list_input)
+        self.assertEqual(list_output, [])
+
+        list_output = Base.from_json_string(None)
+        self.assertEqual(list_output, [])
+
+        self.assertRaises(TypeError, Rectangle.from_json_string, 5)
 
     def test_create(self):
-        """18. Dictionary to Instance
-        TypeError: __init__ for `Base` takes 1 arg, and `create` uses 2 to
-        make `temp`.
-        Testing of `create` done in `TestRectangle` and `TestSquare`.
         """
-        # no args
-        # with any non-keyword args
-        # dict has bad key
-        # dict has too many keys
-        # dict has no keys
-        pass
+        Method for testing the create method in the Base class.
+        """
+        rect1_dict = {'height': 1, 'width': 1, 'id': 400}
+        r1 = self.rect1.create(**rect1_dict)
+        self.assertTrue(type(r1), Rectangle)
+
+        square1_dict = {'size': 1, 'id': 500}
+        s1 = self.square1.create(**square1_dict)
+        self.assertTrue(type(s1), Square)
+
+        self.assertRaises(TypeError, self.square1.create(), 1)
+
+        del r1
+        del s1
 
     def test_load_from_file(self):
-        """19. File to instances
-        `load_from_file` uses `create`, and so must be tested in subclasses
-        Testing of `create` done in `TestRectangle` and `TestSquare`.
         """
-        # Normal use: file with JSON string of instances as dicts
-        # empty file
-        # empty list in file
-        # file has other content / not in JSON
-        # file inaccessible
-        # mixed square and rectangle objects represented in string
-        # any dict has bad key
-        # any dict has too many keys
-        # any dict has no keys
+        Method for testing the load_from_file method in the
+        Base class.
+        """
         pass
 
 if __name__ == '__main__':
