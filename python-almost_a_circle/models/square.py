084358aa725bd4168d65dd0d7d606b006ec9cf45
d4vc-198 <contactodiegoavc@gmail.com>
2023-02-15 14:14:08 -0600

    square modified

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -1,72 +1,52 @@
 #!/usr/bin/python3
-""" Square Module """
+"""square Module"""
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """ Represent Square that inherits from Rectangle """
-
+    """Square class"""
     def __init__(self, size, x=0, y=0, id=None):
-        """ Initialize a new Square """
+        """Initializer for Square class"""
         super().__init__(size, size, x, y, id)
 
-    """ Size """
+    def __str__(self):
+        """return string representation of Rectangle"""
+        return '[' + type(self).__name__ + '] (' + str(self.id) \
+            + ') ' + str(self.x) + '/' + str(self.y) + ' - ' \
+            + str(self.size)
+
+    def update(self, *args, **kwargs):
+        """assigns an argument to each attribute"""
+        key = ["id", "size", "x", "y"]
+        for i in range(len(args)):
+            setattr(self, key[i], args[i])
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+    def to_dictionary(self):
+        """
+        Returns dictionary representation of
+        Rectangle instance
+        """
+        selfDict = {
+            'id': self.id,
+            'x': self.x,
+            'size': self.size,
+            'y': self.y
+        }
+        return selfDict
+
     @property
     def size(self):
+        """Getter for size"""
         return self.width
 
     @size.setter
     def size(self, value):
+        """Setter for __size"""
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
         self.width = value
         self.height = value
-
-    """ UPDATE """
-    def update(self, *args, **kwargs):
-        """Update the Square.
-        Args:
-            *args (ints): New attribute values.
-                - 1st argument represents id attribute
-                - 2nd argument represents size attribute
-                - 3rd argument represents x attribute
-                - 4th argument represents y attribute
-            **kwargs (dict): New key/value pairs of attributes.
-        """
-        if args and len(args) != 0:
-            a = 0
-            for arg in args:
-                if a == 0:
-                    if arg is None:
-                        self.__init__(self.size, self.x, self.y)
-                    else:
-                        self.id = arg
-                elif a == 1:
-                    self.size = arg
-                elif a == 2:
-                    self.x = arg
-                elif a == 3:
-                    self.y = arg
-                a += 1
-
-        elif kwargs and len(kwargs) != 0:
-            for k, v in kwargs.items():
-                if k == "id":
-                    if v is None:
-                        self.__init__(self.size, self.x, self.y)
-                    else:
-                        self.id = v
-                elif k == "size":
-                    self.size = v
-                elif k == "x":
-                    self.x = v
-                elif k == "y":
-                    self.y = v
-
-    """ Dictionary """
-    def to_dictionary(self):
-        """Return the dictionary representation of the Square."""
-        return {
-            "id": self.id,
-            "size": self.width,
-            "x": self.x,
-            "y": self.y
-        }
