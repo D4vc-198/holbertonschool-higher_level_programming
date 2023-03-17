d8160bb77b000b5beb66a973ed4ba13eaf811a2c
d4vc-198 <contactodiegoavc@gmail.com>
2023-03-17 11:12:56 -0600

    0.Get all states

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -6,13 +6,13 @@ import MySQLdb
 
 def main():
     connectionDb = MySQLdb.connect(
-            host="localhost",
-            port=3306,
-            user=sys.argv[1],
-            passwd=sys.argv[2],
-            db=sys.argv[3],
-            charset="utf8"
-            )
+                                host="localhost",
+                                port=3306,
+                                user=sys.argv[1],
+                                passwd=sys.argv[2],
+                                db=sys.argv[3],
+                                charset="utf8"
+                                    )
     cur = connectionDb.cursor()
     query = "SELECT id, name FROM states ORDER by id ASC"
     cur.execute(query)
@@ -21,6 +21,7 @@ def main():
         print(n)
     cur.close()
     connectionDb.close()
+
 
 if __name__ == "__main__":
     main()
