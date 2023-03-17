87093e5edf846df2655025b41d78396a206a8ddb
d4vc-198 <contactodiegoavc@gmail.com>
2023-03-17 10:57:43 -0600

    0.Get all states

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" Module list state """
+import sys
+import MySQLdb
+
+
+def main():
+    connectionDb = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3],
+            charset="utf8"
+            )
+    cur = connectionDb.cursor()
+    query = "SELECT id, name FROM states ORDER by id ASC"
+    cur.execute(query)
+    row = cur.fetchall()
+    for n in row:
+        print(n)
+    cur.close()
+    connectionDb.close()
+
+if __name__ == "__main__":
+    main()
