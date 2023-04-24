3e0d6d93a82c452ff673baa1a44b24b1953d5da3
d4vc-198 <contactodiegoavc@gmail.com>
2023-04-24 15:29:41 -0600

    0.Readme

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,12 +1,11 @@
 #!/usr/bin/node
 
 const fs = require('fs');
-const file = process.argv[0];
+const file = process.argv[2];
 
-fs.readFile(file, 'utf-8', function(err, data) {
+fs.readFile(file, 'utf-8', (err, data) => {
 	if (err) {
 		console.log(err);
-	} else {
-		console.log(data);
 	}
+	process.stdout.write(data);
 });
